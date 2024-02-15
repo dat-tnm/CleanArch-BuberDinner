e84882eff58a1a12fbd4a5a52828a192a4744ccc
dat-tnm <tnmdat183@gmail.com>
2024-02-15 22:26:21 +0700

    implement Global Error Handling

---
+++ BuberDinner.API/Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.API.Controllers
+{
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class ErrorController : ControllerBase
+    {
+        [Route("/error")]
+        public IActionResult Error()
+        {
+            Exception? exception =  HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+
+            return Problem(title: exception?.Message, statusCode: 400);
+        }
+    }
+}
--- BuberDinner.API/Program.cs
+++ BuberDinner.API/Program.cs
@@ -1,9 +1,16 @@
+using BuberDinner.API.Middleware;
+using BuberDinner.API.Middleware.Filter;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
+    //builder.Services.AddControllers(options =>
+    //{
+    //    options.Filters.Add<ErrorHandlingFilterAttribute>();   
+    //});
     builder.Services.AddControllers();
+
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
 
@@ -19,12 +26,10 @@ var app = builder.Build();
         app.UseSwagger();
         app.UseSwaggerUI();
     }
-
+    app.UseExceptionHandler("/error");
+    //app.UseMiddleware<ErrorHandlingMiddleware>();
     app.UseHttpsRedirection();
-
     app.UseAuthorization();
-
     app.MapControllers();
-
     app.Run();
 }
